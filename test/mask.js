b99e430a1eac4916ffd58558083371958d657aeb
jie.ding <jie.ding@msxf.com>
2017-08-01 11:44:47 +0800

    modify encrypt decodeURI handle add try catch

--- test/mask.js
+++ test/mask.js
@@ -2,7 +2,7 @@
 var mask = require('../lib/mask');
 var json = [[{password: '123', 'address': '重庆市九龙坡区手机看看'}, {
     password: '123',
-    'address': '重庆市九龙坡区手机看看',
+    'address': "重庆市%22九龙坡区手机看看",
     data: {password: '123', 'address': '重庆市九龙坡区手机看看'},
     datas: [{password: '123', 'address': '重庆市九龙坡区手机看看'}, {password: '123', 'address': '重庆市九龙坡区手机看看'}]
 }]];
